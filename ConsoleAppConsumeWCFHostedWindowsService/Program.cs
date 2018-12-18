d44ff5ab923087956f4579ba671a083e774b30fa
MOHSEN <mohsenubc@gmail.com>
2018-12-17 23:17:54 -0800

    First Push

---
+++ ConsoleAppConsumeWCFHostedWindowsService/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppConsumeWCFHostedWindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var simpleCalculator = new WCFHostedWindowsServiceClient.SimpleCalculatorClient();
+
+            Console.WriteLine(simpleCalculator.Multiply(100, 10));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WCFHostedWindowsService/WinServiceInstaller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ServiceProcess;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Configuration.Install;
+
+namespace WCFHostedWindowsService
+{
+    [RunInstaller(true)]
+    public class WinServiceInstaller : Installer
+    {
+        private ServiceProcessInstaller process;
+        private ServiceInstaller service;
+
+        public WinServiceInstaller()
+        {
+            process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.NetworkService;
+            service = new ServiceInstaller();
+            service.ServiceName = "WCFHostedWindowsService";
+            service.DisplayName = "WCFHostedWindowsService";
+            service.Description = "WCF Service Hosted";
+            service.StartType = ServiceStartMode.Automatic;
+            Installers.Add(process);
+            Installers.Add(service);
+        }
+    }
+}
