552d121465fb624f10ec94519c0944ef35452079
MOHSEN <mohsenubc@gmail.com>
2019-01-13 15:57:06 -0800

    Revert changes to correct state

--- ConsoleAppConsumeWCFHostedWindowsService/Program.cs
+++ ConsoleAppConsumeWCFHostedWindowsService/Program.cs
@@ -14,16 +14,7 @@ namespace ConsoleAppConsumeWCFHostedWindowsService
 
             Console.WriteLine(simpleCalculator.Multiply(100, 10));
             //This is my local changes to git
-            Console.WriteLine(simpleCalculator.Add(120,40));
-            //This is a remote change to git
-            //System.Threading.Thread.Sleep(1000); //We don't need this command Mohsen, we will add it in next release.
-            var a= "hello";
-            Console.WriteLine($"We Add the last line of the code {a}");
-
-            Console.WriteLine($"Here is my code{a}");
-            //Console.ReadLine();
-
-            Console.ReadLine();
+            Console.WriteLine(simpleCalculator.Add(120, 40));
         }
     }
 }
